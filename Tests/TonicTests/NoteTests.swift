9b45cce7b9942f4dec9e94e5b0b9a11e7991fd6f
Wes Cruver <chieffancypants@gmail.com>
2025-11-05 18:16:22 -0800

    added a few tests for issues found in Tonic

--- Tests/TonicTests/NoteTests.swift
+++ Tests/TonicTests/NoteTests.swift
@@ -52,6 +52,16 @@ final class NoteTests: XCTestCase {
         XCTAssertEqual(cDoubleSharp.description, "C𝄪3")
     }
 
+    func testNoteSpellingBroken() {
+        let Cb = Note(pitch: Pitch(59), key: .Bb)
+        XCTAssertEqual(Cb.description, "C♭3")
+        XCTAssertEqual(Cb.spelling(in: .Bb).description, "C♭")
+
+        let cFlat = Note.Cb
+        XCTAssertEqual(cFlat.description, "C♭3")
+        XCTAssertEqual(cFlat.spelling(in: .Bb).description, "C♭")
+    }
+
     func testComparison() {
         XCTAssert(Note.C < Note.Cs)
     }
